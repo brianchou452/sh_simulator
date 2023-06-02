b7824995e6368a050a9c39f82a40e5964866dd56
brianchou452 <brianchou452@gmail.com>
2023-06-02 22:53:34 +0800

    完成所有功能

--- termbase.c
+++ termbase.c
@@ -1,4 +1,3 @@
-/***** LAB2 base code *****/
 #include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
@@ -12,28 +11,23 @@
 
 #include "logger.h"
 
-#define Close(FD)                                                       \
-  do {                                                                  \
-    int Close_fd = (FD);                                                \
-    if (close(Close_fd) == -1) {                                        \
-      perror("close");                                                  \
-      fprintf(stderr, "%s:%d: close(" #FD ") %d\n", __FILE__, __LINE__, \
-              Close_fd);                                                \
-    }                                                                   \
+#define Close(FD)                                                          \
+  do {                                                                     \
+    int Close_fd = (FD);                                                   \
+    if (close(Close_fd) == -1) {                                           \
+      perror("close");                                                     \
+      LOG_ERROR("%s:%d: close(" #FD ") %d", __FILE__, __LINE__, Close_fd); \
+    }                                                                      \
   } while (0)
 
-char gpath[128];  // hold token strings
-char *args[64];   // token string pointers
-int n;            // number of token strings
-
-char dpath[128];  // hold dir strings in PATH
-char *dir[64];    // dir string pointers
-int ndir = 0;     // number of dirs
+char *args[64];  // token string pointers
 
 int pid, status;
 
 bool redirect_stdout = false;
 char *redirect_stdout_filename;
+bool redirect_stdin = false;
+char *redirect_stdin_filename;
 
 char *commands[64];
 int ncommands = 0;
@@ -43,26 +37,55 @@ int stdout_copy = 0;
 int fd1[2];
 int fd2[2];
 
+/*刪除 command 前後多餘的空格*/
+void trim_white_space(char *str) {
+  int start = 0;
+  int end = strlen(str) - 1;
+  while (str[start] == ' ' || str[start] == '\t' || str[start] == '\n') {
+    start++;
+  }
+  while (str[end] == ' ' || str[end] == '\t' || str[end] == '\n') {
+    end--;
+  }
+  str[end + 1] = '\0';
+  strcpy(str, str + start);
+}
+
+/*
+以空白分割指令
+divide line into token strings
+*/
 int tokenize(char *pathname) {
+  int n;  // number of token strings
   char *s;
   redirect_stdout = false;  // reset
-  strcpy(gpath, pathname);  // copy into global gpath[]
+  redirect_stdin = false;
+  char gpath[128];
+  strcpy(gpath, pathname);  // copy 一份才不會改到原本的
   s = strtok(gpath, " ");
   n = 0;
   while (s) {
-    args[n] = malloc(strlen(s) + 1);  // token string pointers
+    args[n] = malloc(strlen(s) + 1);
     if (strcmp(s, ">") == 0) {
       redirect_stdout = true;
       s = strtok(0, " ");
       redirect_stdout_filename = malloc(strlen(s) + 1);
       strcpy(redirect_stdout_filename, s);
+      LOG_DEBUG("redirect stdout to %s", redirect_stdout_filename);
+      break;
+    } else if (strcmp(s, "<") == 0) {
+      redirect_stdin = true;
+      s = strtok(0, " ");
+      redirect_stdin_filename = malloc(strlen(s) + 1);
+      strcpy(redirect_stdin_filename, s);
+      LOG_DEBUG("redirect stdin from %s", redirect_stdin_filename);
       break;
     }
-    strcpy(args[n], s);  // copy string at s to args[]
+    strcpy(args[n], s);
     n++;
     s = strtok(0, " ");
   }
-  args[n] = NULL;  // name[n] = NULL pointer
+  args[n] = NULL;
 
   for (char **p = args; *p != NULL; p++) {
     LOG_DEBUG("args[%ld] = %s", p - args, *p);
@@ -73,6 +96,8 @@ int tokenize(char *pathname) {
 
 // print argc, argv and env
 void print_debug_info(int argc, char *argv[], char *env[]) {
+  char *dir[64];  // dir string pointers
+  int ndir = 0;   // number of dirs
   LOG_DEBUG("argc: %d", argc);
   for (int i = 0; i < argc; i++) {
     LOG_DEBUG("argv[%d]: %s", i, argv[i]);
@@ -104,19 +129,17 @@ void print_debug_info(int argc, char *argv[], char *env[]) {
   return;
 }
 
+static void report_error_and_exit(const char *msg) {
+  perror(msg);
+  //(child ? _exit : exit)(EXIT_FAILURE);
+  exit(EXIT_FAILURE);
+}
+
+/*執行指令*/
 void exec_command(char *command) {
   LOG_DEBUG("command: %s", command);
-  char *cmd;
 
-  tokenize(command);  // divide line into token strings
-
-  cmd = args[0];
-
-  if (strcmp(cmd, "cd") == 0) {
-    chdir(args[1]);
-    return;
-  }
-  if (strcmp(cmd, "exit") == 0) exit(0);
+  tokenize(command);
 
   fflush(stdout);
   fflush(stdin);
@@ -124,71 +147,63 @@ void exec_command(char *command) {
   if (redirect_stdout) {
     close(1);
     open(redirect_stdout_filename, O_WRONLY | O_CREAT, 0644);
+  } else if (redirect_stdin) {
+    close(0);
+    open(redirect_stdin_filename, O_RDONLY);
   }
 
-  int r = execvp(cmd, args);
+  int r = execvp(args[0], args);
   if (r < 0) {
-    perror("execlp failed");
+    report_error_and_exit("execvp");
   }
 
   exit(1);
-}
-static void report_error_and_exit(const char *msg) {
-  perror(msg);
-  //(child ? _exit : exit)(EXIT_FAILURE);
-  exit(EXIT_FAILURE);
 }
 
 /** move oldfd to newfd */
 static void redirect(int oldfd, int newfd) {
   if (oldfd != newfd) {
     if (dup2(oldfd, newfd) != -1)
-      Close(oldfd); /* successfully redirected */
+      Close(oldfd);
     else
       report_error_and_exit("dup2");
   }
 }
 
-/** execute `cmds[pos]` command and call itself for the rest of the commands.
-    `cmds[]` is NULL-terminate array
-    `exec_pipeline()` never returns.
-*/
-static void exec_pipeline(size_t pos, int in_fd) {
-  if (commands[pos + 1] == NULL) { /* last command */
-    redirect(in_fd, STDIN_FILENO); /* read from in_fd, write to STDOUT */
+/**
+ * 依序執行commands[pos]中的指令，並將前一個指令的輸出導向到下一個指令的輸入
+ */
+static void exec_pipeline(int pos, int in_fd) {
+  if (commands[pos + 1] == NULL) {  // 檢查是否是最後一個指令
+    redirect(in_fd, STDIN_FILENO);  // read from in_fd, write to STDOUT
     exec_command(commands[pos]);
     fflush(stdout);
     fflush(stdin);
     Close(in_fd);
-
-    // execvp(cmds[pos][0], cmds[pos]);
-    // report_error_and_exit("execvp last");
-  } else {     /* $ <in_fd cmds[pos] >fd[1] | <fd[0] cmds[pos+1] ... */
-    int fd[2]; /* output pipe */
+  } else {      // $ <in_fd cmds[pos] >fd[1] | <fd[0] cmds[pos+1] ...
+    int fd[2];  // output pipe
     if (pipe(fd) == -1) report_error_and_exit("pipe");
     switch (fork()) {
       case -1:
         report_error_and_exit("fork");
         break;
-      case 0: /* child: execute current command `cmds[pos]` */
-        // child = 1;
-        Close(fd[0]);                   /* unused */
-        redirect(in_fd, STDIN_FILENO);  /* read from in_fd */
-        redirect(fd[1], STDOUT_FILENO); /* write to fd[1] */
-        // execvp(cmds[pos][0], cmds[pos]);
+      case 0:                            // child
+        Close(fd[0]);                    // unused
+        redirect(in_fd, STDIN_FILENO);   // read from in_fd
+        redirect(fd[1], STDOUT_FILENO);  // write to fd[1]
         exec_command(commands[pos]);
         report_error_and_exit("execvp");
         break;
-      default:        /* parent: execute the rest of the commands */
-        Close(fd[1]); /* unused */
-        Close(in_fd); /* unused */
+      default:         // parent
+        Close(fd[1]);  // unused
+        Close(in_fd);  // unused
 
         LOG_DEBUG("mysh %d forked a child sh %d", getpid(), pid);
         LOG_DEBUG("mysh %d wait for child sh %d to terminate", getpid(), pid);
         wait(&status);
         LOG_DEBUG("ZOMBIE child=%d exitStatus=%x", pid, status);
         LOG_DEBUG("mysh %d repeat loop", getpid());
-        exec_pipeline(pos + 1, fd[0]); /* execute the rest */
+        exec_pipeline(pos + 1, fd[0]);  // 繼續執行剩下的指令
     }
   }
 }
@@ -202,7 +217,9 @@ void split_commands(char *line) {
   s = strtok(line_copy, "|");
   while (s) {
     commands[ncommands] = (char *)malloc(strlen(s) + 1);
-    strcpy(commands[ncommands++], s);
+    strcpy(commands[ncommands], s);
+    trim_white_space(commands[ncommands]);
+    ncommands++;
     s = strtok(0, "|");
   }
   commands[ncommands] = NULL;
@@ -212,7 +229,6 @@ void split_commands(char *line) {
 }
 
 int main(int argc, char *argv[], char *env[]) {
-  char *cmd;
   char line[128];
 
   stdin_copy = dup(STDIN_FILENO);
@@ -220,63 +236,55 @@ int main(int argc, char *argv[], char *env[]) {
 
   LOG_DEBUG("Starting program: %s", __FILENAME__);
 
-  // print_debug_info(argc, argv, env);
+  print_debug_info(argc, argv, env);
 
   while (1) {
+    char cwd[128];
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+      report_error_and_exit("getcwd");
+    }
+
     LOG_DEBUG("mysh %d running", getpid());
-    printf("enter a command line : ");  // cat file1 file2
-                                        // ls -l -a -f
-                                        // cat file | grep print
-                                        // ANY valid Linux command line
-    fgets(line, 128, stdin);
+    printf("%s > ",
+           (__builtin_strrchr(cwd, '/') ? __builtin_strrchr(cwd, '/') + 1
+                                        : cwd));  // print prompt
+
+    fgets(line, 128, stdin);     // Accept ANY valid Linux command line
     line[strlen(line) - 1] = 0;  // kill \n at end
 
-    if (line[0] == '\0')  // if (strcmp(line, "")==0) // if line is NULL
+    if (line[0] == '\0')  // if line is NULL
       continue;
     fflush(stdin);
     split_commands(line);
-    // split_commands("cat animal.txt|more");
-    //  recursive_pipe(ncommands - 1);
-    if (signal(SIGCHLD, SIG_IGN) == SIG_ERR) /* POSIX.1-2001 reap children */
-      report_error_and_exit("signal");
-    // exec_pipeline(0, STDIN_FILENO);
 
-    switch (fork()) {
+    if (strstr(commands[0], "cd") != NULL) {
+      tokenize(commands[0]);
+      chdir(args[1]);
+      continue;
+    }
+    if (strstr(commands[0], "exit") != NULL) exit(0);
+
+    /*在POSIX.1-2001標準規定中，父行程可將SIGCHLD的處理常式設為SIG_IGN（亦為預設設定），
+    或為SIGCHLD設定SA_NOCLDWAIT標記，以使Kernel可以自動回收已終止的子行程的資源。*/
+    if (signal(SIGCHLD, SIG_IGN) == SIG_ERR) report_error_and_exit("signal");
+
+    pid = fork();
+    switch (pid) {
       case -1:
         report_error_and_exit("fork");
         break;
-      case 0: /* child: execute current command `cmds[pos]` */
+      case 0:  // child
         exec_pipeline(0, STDIN_FILENO);
         report_error_and_exit("execvp");
         break;
-      default: /* parent: execute the rest of the commands */
+      default:  // parent
         LOG_DEBUG("mysh %d forked a child sh %d", getpid(), pid);
         LOG_DEBUG("mysh %d wait for child sh %d to terminate", getpid(), pid);
         wait(&status);
-        LOG_DEBUG("ZOMBIE child=%d exitStatus=%x", pid, status);
+        LOG_DEBUG("ZOMBIE child = %d exitStatus = %x", pid, status);
         LOG_DEBUG("mysh %d repeat loop", getpid());
     }
   }
 
   return 0;
 }
-
-/********************* YOU DO ***********************
-1. I/O redirections:
-
-Example: line = arg0 arg1 ... > argn-1
-
-  check each arg[i]:
-  if arg[i] = ">" {
-     arg[i] = 0; // null terminated arg[ ] array
-     // do output redirection to arg[i+1] as in Page 131 of BOOK
-  }
-  Then execve() to change image
-
-
-2. Pipes:
-
-Single pipe   : cmd1 | cmd2 :  Chapter 3.10.3, 3.11.2
-
-Multiple pipes: Chapter 3.11.2
-****************************************************/
